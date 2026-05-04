871c1f94dde56523ae6fcdbe0d195b17879defb6
Apple <apple@Apples-MacBook-Air.local>
2026-05-04 15:03:10 +0530

    final app entry fix

--- manage.py
+++ manage.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
 import os
 import sys
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ScholarDesk.settings")
+
+from django.core.wsgi import get_wsgi_application
+
+application = get_wsgi_application()
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ScholarDesk.settings')
